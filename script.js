43ca703383c40f142b4f14a719ca6ce3147ab23d
Sourabkansal <sourabkansal56@gmail.com>
2024-09-30 00:12:39 +0530

    add time tables

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 let toogle=()=>{
     let ul = document.querySelector('.ul1');
     ul.classList.toggle('ul2');
-    
-  
+
+}
+let navbackclick =()=>{
+    if(ul.classList.contains('ul2')){
+        let a = document.querySelector(".maindiv");
+        a.classList.add(".backclick")
+    }
 }
