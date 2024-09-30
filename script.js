a984dc55e3d51fde4dd10768bb5f22928e8098eb
Sourabkansal <sourabkansal56@gmail.com>
2024-09-30 23:16:49 +0530

    add navBACk click

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ let toogle=()=>{
     ul.classList.toggle('ul2');
 
 }
-let navbackclick =()=>{
-    if(ul.classList.contains('ul2')){
-        let a = document.querySelector(".maindiv");
-        a.classList.add(".backclick")
-    }
+// let navbackclick =()=>{
+//     if(ul.classList.contains('ul2')){
+//         let a = document.querySelector(".maindiv");
+//         a.classList.add(".backclick")
+//     }
+
+ function navbackclick(){
+    let ul = document.querySelector('.ul1');
+    ul.classList.toggle('ul2');
 }
